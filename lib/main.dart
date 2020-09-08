aaf69f15d3fd13b6d3d12ac073a8b76afbe889d9
Matthaus Nawan <matthausnawan@gmail.com>
2020-09-07 22:38:21 -0300

    lists flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,62 @@
 import "package:flutter/material.dart";
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MeuApp());
 
 class MeuApp extends StatelessWidget {
-  callAction() {}
-
-  mapAction() {}
-
-  shareAction() {}
+  var contatos = [
+    'Matthaus Nawan',
+    'Aline Raquel',
+    'Pedro Gael',
+    'Samuel Levi'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-            brightness: Brightness.light,
-            primaryColor: Colors.green[400],
-            textTheme: TextTheme(body1: TextStyle(color: Colors.grey)),
-            primaryTextTheme: TextTheme(title: TextStyle(color: Colors.white))),
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Meu Primeiro App com Flutter'),
-          ),
-          body: Column(
-            children: [
-              Image(
-                image: AssetImage('assets/muro.jpg'),
-              ),
-              Container(
-                color: Colors.red[50],
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Kotel',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black)),
-                          Text('Jerusalem, Israel',
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.grey))
-                        ],
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.star, color: Colors.blue),
-                        Text('9.876')
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  children: [
-                    Botao(
-                      icon: Icons.call,
-                      text: 'Ligar',
-                      onPress: callAction,
-                    ),
-                    Botao(
-                        icon: Icons.location_on,
-                        text: 'Ver no Mapa',
-                        onPress: mapAction),
-                    Botao(
-                        icon: Icons.share,
-                        text: 'Compartilhar',
-                        onPress: shareAction)
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  'O Muro das Lamentações ou Muro Ocidental é o segundo local mais sagrado do judaísmo, atrás somente do Santo dos Santos, no monte do Templo. Trata-se do único vestígio do antigo Templo de Herodes, erguido por Herodes, o Grande no lugar do Templo de Jerusalém inicial.',
-                ),
-              )
-            ],
-          ),
-        ));
-  }
-}
-
-class Botao extends StatelessWidget {
-  Botao({this.onPress, @required this.icon, @required this.text});
-
-  IconData icon;
-  Function onPress;
-  var text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: FlatButton(
-        onPressed: onPress,
-        child: Column(
-          children: [Icon(icon, color: Colors.blue), Text(text)],
-        ),
-      ),
-    );
+            appBar: AppBar(title: Text('Flutter e Listas')),
+            //LISTA DINAMICA
+            body: ListView.builder(
+              itemCount: contatos.length,
+              itemBuilder: (ctx, index) {
+                return ListTile(
+                  title: Text('${contatos[index]}'),
+                  leading: Icon(Icons.person),
+                  trailing: FlatButton(
+                    onPressed: () {},
+                    child: Icon(Icons.call),
+                  ),
+                );
+              },
+            )
+            //GRID VIEW
+            // body: GridView.count(
+            //   crossAxisCount: 2,
+            //   scrollDirection: Axis.vertical,
+            //   children: [
+            //     Container(color: Colors.red, margin: EdgeInsets.all(5)),
+            //     Container(color: Colors.red[100], margin: EdgeInsets.all(5)),
+            //     Container(color: Colors.blue[100], margin: EdgeInsets.all(5)),
+            // Container(color: Colors.blue[100]),
+            // ],
+            // )
+            //LIST VIEW
+            //ListView(scrollDirection: Axis.horizontal, children: [
+            // ListTile(
+            //   title: Text('Mapa'),
+            //   leading: Icon(Icons.map),
+            //   trailing: Icon(Icons.check),
+            // ),
+            // ListTile(
+            //   title: Text('configs'),
+            //   leading: Icon(Icons.settings),
+            //   trailing: Icon(Icons.check),
+            // ),
+            //Container(width: 150, color: Colors.red),
+            //Container(width: 150, color: Colors.blue),
+            //Container(width: 150, color: Colors.green)
+            //]),
+            ));
   }
 }
